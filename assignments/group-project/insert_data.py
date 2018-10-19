361c4f38065735dbc166514fafb44b15f932b6f7
HenryFBP <HenryFBP@gmail.com>
2018-10-18 19:29:44 -0500

    add small fix for blank password

--- assignments/group-project/insert_data.py
+++ assignments/group-project/insert_data.py
@@ -3,7 +3,7 @@ import xlrd
 
 PROJECT_DIR = os.path.dirname(__file__)
 LOGIN_FILE_NAME = 'login_info.txt'
-SERVER_IP = '127.0.0.1' # I'm assuming you're running this locally.
+SERVER_IP = '127.0.0.1'  # I'm assuming you're running this locally.
 
 
 def get_login_creds(path: str) -> (str, str,):
@@ -14,8 +14,17 @@ def get_login_creds(path: str) -> (str, str,):
     except FileNotFoundError as e:
         print(f"Please make {LOGIN_FILE_NAME}, and put your username and password in it, separated by a newline.")
         exit(1)
+    except Exception as e:
+        print(f"Not sure what went wrong.")
+        print(f"Make sure all the paths and locations are right and try again.")
+        print(f"Also, send this error to me, please:")
+        print(e)
 
     lines = file.readlines()
+
+    # Fix for blank password.
+    if (len(lines) is 1) and ('\n' in lines[0]):
+        lines = lines[0].split('\n')
 
     if len(lines) < 2:
         print(f"There's less than two lines in {LOGIN_FILE_NAME}.")
