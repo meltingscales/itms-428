b6807586103f5ec81f5dffb1e01a0629edd3c478
HenryFBP <HenryFBP@gmail.com>
2018-11-01 14:16:21 -0500

    begin login system

--- assignments/group-project/constants.py
+++ assignments/group-project/constants.py
@@ -6,4 +6,4 @@ class Config(object):
     LOGIN_FILE_NAME = 'login_info.txt'
     SERVER_IP = '127.0.0.1'  # I'm assuming you're running this locally.
     DATABASE_NAME = 'itms428'
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'this_is_not_secure_so_please_dont_do_it_in_a_production_server_k_thx_bai')
--- assignments/group-project/flask/cool_app.py
+++ assignments/group-project/flask/cool_app.py
@@ -2,11 +2,13 @@ import sys
 
 from flaskext.mysql import MySQL
 
+from forms import LoginForm
+
 sys.path.append('..')  # Allows us to import stuff in above folder.
 
 from data.mysql_sample_db import UsersDatabase
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from shared_lib import get_login_creds
 from constants import *
 
@@ -27,8 +29,46 @@ mysql.init_app(app)
 
 connection = mysql.connect()
 
-def login(username:str, password:str) -> bool:
-    return False
+
+def user_exists(username: str) -> bool:
+    cursor = connection.cursor()
+
+    cursor.execute(f"""
+    SELECT
+        COUNT(*)
+    FROM 
+        {UsersDatabase.table_name}
+    WHERE 
+        username LIKE "{username}" """)
+
+    x = cursor.fetchone()
+
+    cursor.close()
+
+    return x[0] > 0
+
+
+def login(username: str, password: str) -> bool:
+    """ Does this username and password combination exist? """
+
+    cursor = connection.cursor()
+
+    cursor.execute(f"""
+        SELECT
+            COUNT(*)
+        FROM
+            {UsersDatabase.table_name}
+        WHERE 
+            username LIKE "{username}" 
+                AND
+            password LIKE "{password}";""")
+
+    x = cursor.fetchone()
+
+    cursor.close()
+
+    return x[0] > 0
+
 
 @app.route("/")
 def main():
@@ -74,9 +114,28 @@ def admin():
     data[UsersDatabase.table_name]['header'] = results
 
     cursor.close()
-
     return render_template('data.html', data=data)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+
+        return redirect('../')
+
+    return render_template('login.html', title="Sign in", form=form)
 
 
 if __name__ == '__main__':
     app.run()
+
+    # Sanity checks.
+    assert (login("henry", "iliketofarm"))
+    assert (not login("henry", "notafarmer"))
+
+    assert (user_exists("henry"))
+    assert (not user_exists("tiffany_the_lord_of_squids"))
