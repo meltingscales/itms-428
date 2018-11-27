c011c9a231ade5e525618b870e0d6deedcc618a4
HenryFBP <HenryFBP@gmail.com>
2018-11-27 15:52:05 -0600

    reformat cool_app.py

--- assignments/group-project/flask/cool_app.py
+++ assignments/group-project/flask/cool_app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import  mysql_helper
+import mysql_helper
 
 from flaskext.mysql import MySQL
 
@@ -61,22 +61,22 @@ def admin():
     return render_template('data.html', data=dump_table_to_dict(UsersDatabase.table_name, connection))
 
 
-superadmin=['henry', 'shephalika']
-dataadmin=['sunil', 'cody', 'dennis', 'sridhar']
+superadmin = ['henry', 'shephalika']
+dataadmin = ['sunil', 'cody', 'dennis', 'sridhar']
 
-sysusenotification="******** This system is for the use of authorized users only."\
-                   "Individuals using this computer system without"\
-                   " authority, or in excess of their authority, are subject"\
-                   " to having all of their activities on this system"\
-                   " monitored and recorded by system personnel.  In the"\
-                   " course of monitoring individuals improperly using this"\
-                   " system, or in the course of system maintenance, the"\
-                   " activities of authorized users may also be monitored. "\
-                   " Anyone using this system expressly consents to such"\
-                   " monitoring and is advised that if such monitoring"\
-                   " reveals possible evidence of criminal activity, system"\
-                   " personnel may provide the evidence of such monitoring"\
-                   " to law enforcement officials. **********"
+sysusenotification = """******** This system is for the use of authorized users only.
+Individuals using this computer system without
+authority, or in excess of their authority, are subject
+to having all of their activities on this system
+monitored and recorded by system personnel. In the
+course of monitoring individuals improperly using this
+system, or in the course of system maintenance, the
+activities of authorized users may also be monitored. 
+Anyone using this system expressly consents to such
+monitoring and is advised that if such monitoring
+reveals possible evidence of criminal activity, system
+personnel may provide the evidence of such monitoring
+to law enforcement officials. **********"""
 
 
 @app.route('/login', methods=['GET', 'POST'])
